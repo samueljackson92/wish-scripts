cea8a5b4769917fcf2ec9876e077f49aaf60162d
Jackson <lhs18285@ndmlnx01.isis.cclrc.ac.uk>
2016-10-13 14:32:02 +0100

    Update reduction scripts

--- SRO_focus.py
+++ SRO_focus.py
@@ -1,10 +1,15 @@
 from mantid.simpleapi import *
 import os.path
 
+# Script Parameters
+# ---------------------------------------------------------------------------------------
+
 path = "/archive/ndxwish/Instrument/data/cycle_16_3/"
 output_path = os.path.join(os.path.expanduser('~'), 'data')
 grouping_filename = os.path.join(os.path.expanduser('~'), 'cal_files/35922_h00_RW.cal')
 focused_suffix = '-foc-h00'
+
+monitor_id = 4
 crop_limits = {"XMin": 6000, "XMax": 99900 }
 
 run_numbers = []
@@ -12,25 +17,38 @@ run_numbers.append(35991)
 run_numbers.extend(range(35979,35983))
 run_numbers.append(35988)
 run_numbers.extend(range(35983,35985))
-#run_numbers = range(35992,35994)
 
+min_run = min(map(lambda x: x if type(x) == int else min(x), run_numbers))
+max_run = max(map(lambda x: x if type(x) == int else max(x), run_numbers))
+group_name = str(min_run) + '-' + str(max_run) + focused_suffix
+
+# ---------------------------------------------------------------------------------------
+
+focused_workspaces = []
 for runno in run_numbers:
     run_number = str(runno)
     
-    raw_file = os.path.join(path, 'WISH000'+run_number+'.raw')
-    LoadRaw(Filename=raw_file, OutputWorkspace=run_number, LoadLogFiles=False)
-    CropWorkspace(InputWorkspace=run_number, OutputWorkspace=run_number, **crop_limits)
+    raw_ws = 'WISH000'+run_number
+    raw_file = os.path.join(path, raw_ws +'.raw')
+    
+    LoadRaw(Filename=raw_file, OutputWorkspace=raw_ws)
+    CropWorkspace(InputWorkspace=raw_ws, OutputWorkspace=run_number, **crop_limits)
     NormaliseByCurrent(InputWorkspace=run_number, OutputWorkspace=run_number)
     ConvertUnits(InputWorkspace=run_number, OutputWorkspace=run_number, Target='Wavelength')
-    NormaliseToMonitor(InputWorkspace=run_number, OutputWorkspace=run_number, MonitorID=4)
+    NormaliseToMonitor(InputWorkspace=run_number, OutputWorkspace=run_number, MonitorID=monitor_id)
     ConvertUnits(InputWorkspace=run_number, OutputWorkspace=run_number, Target='dSpacing')
     
     focused = run_number + focused_suffix
     focused_xye = focused +'.dat'
     focused_nxs = focused +'.nxs'
     DiffractionFocussing(InputWorkspace=run_number, OutputWorkspace=focused, GroupingFileName=grouping_filename, PreserveEvents=False)
+
     SaveFocusedXYE(focused, os.path.join(output_path, focused_xye))
     SaveNexusProcessed(InputWorkspace=focused, Filename=os.path.join(output_path, focused_nxs))
 
+    focused_workspaces.append(focused)
+    DeleteWorkspace(raw_ws)
     DeleteWorkspace(run_number)
+    
+GroupWorkspaces(focused_workspaces, OutputWorkspace=group_name)
     
--- integration_plus.py
+++ integration_plus.py
@@ -1,13 +1,8 @@
 from mantid.simpleapi import *
-import os
+import numpy as np
 
-suffix = "-foc-h00"
-integrate_suffix = "-int"
-
-B = 7.85
-T = [1.2,0.9,0.7,0.5,0.3,0.2,0.1,0.05]
-
-integration_range = {"RangeLower": 16.2, "RangeUpper": 17.2}
+# Script Parameters
+# ---------------------------------------------------------------------------------------
 
 run_numbers = []
 run_numbers.append(35991)
@@ -15,54 +10,87 @@ run_numbers.extend(range(35979,35983))
 run_numbers.append(35988)
 run_numbers.extend(range(35983,35985))
 
+suffix = "-foc-h00"
+integrate_suffix = "-int"
+
+min_run = min(map(lambda x: x if type(x) == int else min(x), run_numbers))
+max_run = max(map(lambda x: x if type(x) == int else max(x), run_numbers))
+group_name = str(min_run) + '-' + str(max_run) + suffix + integrate_suffix
+
+table_name = "Ei=3.5meV (0.219,0,0)"
+log_names = ["MC_temp", "Tesl_setB"]
+integration_range = {"RangeLower": 16.2, "RangeUpper": 17.2}
+
+# ---------------------------------------------------------------------------------------
+
 def create_table(name, columns, num_rows):
+    """ Create an empty table workspace with the given columns """
+    # build table with log names
     table = CreateEmptyTableWorkspace(OutputWorkspace=name)
     for i, c in enumerate(columns):
-        table.addColumn('float', c) 
+        table.addColumn('float', c)
+        table.setPlotType(c, 1)
+     
+    # Add columns for data from workspace last
+    table.addColumn('float', 'int')
+    table.setPlotType('int', 2)
+    table.addColumn('float', 'error')
+    table.setPlotType('error', 5)
     return table
 
-def generate_merged_name(runs, suffix):
-    name = suffix 
-    for r in runs:
-        name = str(r) + "&" + name
-    return name
-
 def convert_run_to_name(run_number, suffix):
+    """ Convert a list of run numbers to workspace names
+   
+    The name will be <runno><suffix>. This can handle
+    the case of sublists of run numbers.
+    """
     convert_name = lambda r: str(r) + suffix
     if type(run_number) == list:
         return [convert_name(r) for r in run_number]
     else:
         return convert_name(run_number) 
 
+def get_log(ws, name):
+    """ Get a log from a workspace.
+    
+    If the log contains multiple values then the
+    mean of the values will be returned.
+    """
+    logs = ws.getRun()
+    prop = logs.getProperty(name)
+    if isinstance(prop.value, np.ndarray):
+        return prop.value.mean()
+    else:
+        return prop.value
 
-column_names = ["T", "B", "int", "errors"]
-table = create_table("Ei=3.5meV (0.219,0,0)", column_names, len(run_numbers))
+table = create_table(table_name, log_names, len(run_numbers))
 run_names = map(lambda x: convert_run_to_name(x, suffix), run_numbers)
 
+output_names = []
 for i, run in enumerate(run_names):
     if type(run) == list:
-        # generate name of OutputWorkspace for Plus function
-        output_workspace = generate_merged_name(run, suffix)
-        
-        #average over each of the of the workspaces
-        first_run = run[0]
-        second_run = run[1]
-        Plus(LHSWorkspace=first_run, RHSWorkspace=second_run, OutputWorkspace=output_workspace)
-        if len(run) > 2:
-            for run_name in run[2:]:
-                Plus(LHSWorkspace=output_workspace, RHSWorkspace=run_name, OutputWorkspace=output_workspace)
-        Scale(InputWorkspace=output_workspace, OutputWorkspace=output_workspace, Factor=1.0/len(run) )
+        # multiple runs, average over all of them
+        output_workspace = '&'.join(run) + integrate_suffix
+        Mean(','.join(run), OutputWorkspace=output_workspace)
     else:
+        # single run, use name as is
         output_workspace = run
 
-    # finally, integrate the run
-    integrated_workspace = run + integrate_suffix
+    #integrate the run
+    integrated_workspace = output_workspace + integrate_suffix
     Integration(InputWorkspace=output_workspace, OutputWorkspace=integrated_workspace, **integration_range)
     w1=mtd[integrated_workspace]
 
-    row = [ T[i], B, w1.readY(0), w1.readE(0) ]
+    # add to table
+    row = [get_log(w1, name) for name in log_names]
+    row .extend([w1.readY(0), w1.readE(0) ])
     row = map(float, row)
     table.addRow(row)
+    
+    # add to workspace group
+    output_names.append(integrated_workspace)
+    
+GroupWorkspaces(output_names, OutputWorkspace=group_name)
 
 #Plus(LHSWorkspace='35943-foc-h00', RHSWorkspace='35922-foc-h00', OutputWorkspace='test')
 #Scale(InputWorkspace='test', OutputWorkspace='test', Factor=0.5)
